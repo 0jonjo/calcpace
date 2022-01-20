271a8b4cca864c65bd5ac96dacd9c27099186564
Jonjo <jgilbertons@gmail.com>
2022-01-20 19:42:24 -0300

    create Run class attributes

--- lib/ruby-calcpace.rb
+++ lib/ruby-calcpace.rb
@@ -1,5 +1,11 @@
 class Run
 
+  attr_accessor :time, :pace, :distance
+
+  def initialize
+    @time, @pace, @distance = 0
+  end
+
   def convert_to_seconds(time)
     hour, minute, seconds = time.split(':')
     adjustedtime = ((hour.to_i)*3600) + ((minute.to_i)*60) + seconds.to_i
@@ -10,15 +16,15 @@ class Run
   end
 
   def calculate_pace(time, distance)
-    pace = time / distance 
+    @pace = time / distance 
   end
 
   def calculate_timerun(pace, distance)
-    pace = pace * distance 
+    @time = pace * distance 
   end
 
   def calculate_distance(pace, time)
-    distance = time / pace
+    @distance = time / pace
   end  
 
 end    
