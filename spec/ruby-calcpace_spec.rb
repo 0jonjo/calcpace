ddd91141bf37166e63c2f8cdc90deae8ce260f13
Jonjo <jgilbertons@gmail.com>
2022-02-05 21:28:51 -0300

    refactoring test remove variable run

--- spec/ruby-calcpace_spec.rb
+++ spec/ruby-calcpace_spec.rb
@@ -3,28 +3,23 @@ require './lib/ruby-calcpace.rb'
 
 describe Run do
   it "convert a run time to seconds" do
-    run = Run.new
-    expect(run.convert_to_seconds("01:11:02")).to eq(4262)
+    expect(Run.new.convert_to_seconds("01:11:02")).to eq(4262)
   end
 
   it "convert seconds to a clocktime" do
-    run = Run.new
-    expect(run.convert_to_clocktime(4262)).to eq("01:11:02")
+    expect(Run.new.convert_to_clocktime(4262)).to eq("01:11:02")
   end
   
   it "calculate a pace" do
-    run = Run.new
-    expect(run.calculate_pace(3600, 10)).to eq(360)
+    expect(Run.new.calculate_pace(3600, 10)).to eq(360)
   end  
 
   it "calculate a time run" do
-    run = Run.new
-    expect(run.calculate_timerun(360, 10)).to eq(3600)
+    expect(Run.new.calculate_timerun(360, 10)).to eq(3600)
   end  
 
   it "calculate a distance" do
-    run = Run.new
-    expect(run.calculate_distance(3600, 360)).to eq(10)
+    expect(Run.new.calculate_distance(3600, 360)).to eq(10)
   end  
 
   it "recieve all run informations" do
