cf59978ed68785440fb9618d8deb2162b433488b
Jonjo <jgilbertons@gmail.com>
2022-02-06 17:02:12 -0300

    adjust atrr_reader and edit methods and create tests to negative distance and seconds

--- lib/ruby-calcpace.rb
+++ lib/ruby-calcpace.rb
@@ -1,29 +1,56 @@
 class Run
 
-  attr_accessor :time, :pace, :distance
+  attr_reader :time, :pace, :distance
 
+  def time(time_run)
+    @time = time_run
+  end
+
+  def pace(pace_run)
+    @pace = pace_run
+  end
+
+  def distance(distance_run)
+    if distance_run.negative? || distance_run.zero?
+      raise "Distance can't be zero or negative"  
+    else
+      @distance = distance_run
+    end
+  end  
+  
   def convert_to_seconds(time)
     hour, minute, seconds = time.split(':')
     adjustedtime = ((hour.to_i)*3600) + ((minute.to_i)*60) + seconds.to_i
   end
 
   def convert_to_clocktime(seconds)
-    Time.at(seconds).utc.strftime("%H:%M:%S")
+    if seconds.negative?
+      raise "Clocktime can't be zero or negative"
+    else  
+      Time.at(seconds).utc.strftime("%H:%M:%S")
+    end
   end
 
   def calculate_pace(time, distance)
-    pace = time / distance 
+    @pace = time / distance 
   end
 
   def calculate_timerun(pace, distance)
-    time = pace * distance 
+    @time = pace * distance 
   end
 
   def calculate_distance(time, pace)
-    distance = time / pace
+    @distance = time / pace
   end  
 
   def informations_to_print
-    "You ran #{distance} km in #{time} at #{pace} pace."
+    "You ran #{@distance} km in #{@time} at #{@pace} pace."
   end
 end    
+
+# run = Run.new
+# run.time("01:00:00")
+# run.pace("00:06:00")
+#run.distance(10) 
+
+# p run.time 
--- spec/ruby-calcpace_spec.rb
+++ spec/ruby-calcpace_spec.rb
@@ -9,7 +9,11 @@ describe Run do
   it "convert seconds to a clocktime" do
     expect(Run.new.convert_to_clocktime(4262)).to eq("01:11:02")
   end
-  
+
+  it "convert seconds to a clocktime error" do
+    expect{Run.new.convert_to_clocktime(-331)}.to raise_error("Clocktime can't be negative")
+  end
+
   it "calculate a pace" do
     expect(Run.new.calculate_pace(3600, 10)).to eq(360)
   end  
@@ -22,11 +26,19 @@ describe Run do
     expect(Run.new.calculate_distance(3600, 360)).to eq(10)
   end  
 
+  it "calculate a distance negative error" do
+    expect{Run.new.distance(-10)}.to raise_error("Distance can't be negative")
+  end
+
   it "recieve all run informations" do
     run = Run.new
-    run.time = "01:00:00"
-    run.pace = "00:06:00"
-    run.distance = 10 
+    run.time("01:00:00")
+    run.pace("00:06:00")
+    run.distance(10) 
     expect(run.informations_to_print).to eq("You ran 10 km in 01:00:00 at 00:06:00 pace.")
-  end                                       
+  end   
+  
+  # it "" do
+  #  expect().to eq()
+  # end
 end
