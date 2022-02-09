46635cf11c287d258b0f958a665af1d3bee43115
Jonjo <jgilbertons@gmail.com>
2022-02-09 09:17:59 -0300

    refactoring calculator

--- lib/calculator.rb
+++ lib/calculator.rb
@@ -3,20 +3,16 @@ require_relative 'ruby-calcpace'
 run = Run.new
 
 puts "Insert time run (XX:XX:XX) or ENTER"
-gets_time = gets.chomp
-run.set_time(run.convert_to_seconds(gets_time))
+run.set_time(run.convert_to_seconds(gets.chomp))
 puts "Insert distance (XX.XX) km or ENTER"
 run.set_distance(gets.chomp.to_f)
 puts "Insert pace (XX:XX:XX) or ENTER"
-gets_pace = gets.chomp
-run.set_pace(run.convert_to_seconds(gets_pace))
+run.set_pace(run.convert_to_seconds(gets.chomp))
 
-if gets_pace.empty?
-  run_pace_calculate = run.calculate_pace(run.time, run.distance)
-  run.set_pace(run_pace_calculate)
-elsif gets_time.empty?
-  run_time_calculate = run.calculate_timerun(run.pace, run_distance)
-  run.set_time(run_time_calculate)
+if run.pace.zero?
+  run.set_pace(run.calculate_pace(run.time, run.distance))
+elsif run.time.zero?
+  run.set_time(run.calculate_timerun(run.pace, run_distance))
 elsif run.distance.zero?
   run.set_distance(run.calculate_distance(run.time, run.pace))
 else 
