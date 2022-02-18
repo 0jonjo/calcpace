01a5eb99984b86c1d84be141dcf1f9a575ead7f9
Jonjo <jgilbertons@gmail.com>
2022-02-17 21:35:59 -0300

    rename src to lib because rspec stantard config

---
+++ lib/main.rb
@@ -0,0 +1,17 @@
+require_relative 'run'
+
+run = Run.new(0, 0, 0, false)
+
+puts "Insert any letter for MPH or ENTER for km"
+run.set_mph(gets.chomp.empty? ? false : true)
+
+puts "Insert time run (XX:XX:XX) or ENTER"
+run.set_time(Run.convert_to_seconds(gets.chomp))
+puts "Insert distance (XX.XX) or ENTER"
+run.set_distance(gets.chomp.to_f)
+puts "Insert pace (XX:XX:XX) or ENTER"
+run.set_pace(Run.convert_to_seconds(gets.chomp))
+
+run.choose_calculus
+
+puts run
