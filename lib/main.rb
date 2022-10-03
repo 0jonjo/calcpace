54c39b15943605b345ea10b233a63db7d33ab200
Jonjo <jgilbertons@gmail.com>
2022-10-03 17:00:20 -0300

    check time if XX:XX or XX-XX

--- lib/main.rb
+++ lib/main.rb
@@ -6,11 +6,11 @@ puts "Insert any letter for MPH or ENTER for km"
 run.set_mph(gets.chomp.empty? ? false : true)
 
 puts "Insert time run (XX:XX:XX) or ENTER"
-run.set_time(Run.convert_to_seconds(gets.chomp))
+run.set_time(Run.convert_to_seconds(Run.check_digits_time(gets.chomp)))
 puts "Insert distance (XX.XX) or ENTER"
 run.set_distance(Run.check_digits_distance(gets.chomp))
 puts "Insert pace (XX:XX:XX) or ENTER"
-run.set_pace(Run.convert_to_seconds(gets.chomp))
+run.set_pace(Run.convert_to_seconds(Run.check_digits_time(gets.chomp)))
 
 run.choose_calculus
 
