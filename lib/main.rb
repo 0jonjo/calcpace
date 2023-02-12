47e072085c8482bb911d5430fb7d3a2c81f5aaf3
Jonjo <jgilbertons@gmail.com>
2023-02-11 23:05:28 -0300

    Remove duplicate lines and methods

--- lib/main.rb
+++ lib/main.rb
@@ -1,27 +1,26 @@
-require_relative 'run_calc'
 require_relative 'run_convert'
 require_relative 'run_check'
 
 check_argv_length(ARGV)
+check_digits_time(ARGV[1])
+time_or_pace = convert_to_seconds((ARGV[1]))
+distance_or_pace = ARGV[2]
 
 case ARGV[0][0].downcase
 when "p"
-  run_time = convert_to_seconds(check_digits_time(ARGV[1]))
-  run_distance = check_digits_distance(ARGV[2])
-  run_pace = calculate_pace(run_time, run_distance).to_i
-  raise_negative(run_pace)
-  puts convert_to_clocktime(run_pace)
+  check_digits_distance(distance_or_pace)
+  pace = (time_or_pace / distance_or_pace.to_i)
+  raise_negative(pace)
+  puts convert_to_clocktime(pace)
 when "t"
-  run_pace = convert_to_seconds(check_digits_time(ARGV[1]))
-  run_distance = check_digits_distance(ARGV[2])
-  run_time = calculate_timerun(run_pace, run_distance).to_i
-  raise_negative(run_time)
-  puts convert_to_clocktime(run_time)
+  check_digits_distance(distance_or_pace)
+  time = (time_or_pace * distance_or_pace.to_i)
+  raise_negative(time)
+  puts convert_to_clocktime(time)
 when "d"
-  run_time = convert_to_seconds(check_digits_time(ARGV[1]))
-  run_pace = convert_to_seconds(check_digits_time(ARGV[2]))
-  run_distance = calculate_distance(run_time, run_pace)
-  puts run_distance
+  check_digits_time(distance_or_pace)
+  distance = (time_or_pace.to_f) / convert_to_seconds(distance_or_pace)
+  puts distance.round(2)
 else
   raise ArgumentError, "You have to choose p (pace), t (time run) or d (distance)."  
 end
