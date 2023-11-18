cf3b9b17245b22e9fefccb8b793defd8bdedec62
Joao Gilberto Saraiva <jgilbertons@gmail.com>
2023-11-18 16:56:18 -0300

    rubocop: minor changes

--- spec/run_convert_spec.rb
+++ spec/run_convert_spec.rb
@@ -1,15 +1,15 @@
-require "run_convert"
+# frozen_string_literal: true
 
-describe "run_convert" do
+require 'run_convert'
 
-  context "Convert tests" do
-
-    it ":convert_to_seconds" do
-      expect(convert_to_seconds("01:11:02")).to eq(4262)
+describe 'run_convert' do
+  context 'Convert tests' do
+    it ':convert_to_seconds' do
+      expect(convert_to_seconds('01:11:02')).to eq(4262)
     end
 
-    it ":convert_to_clocktime" do
-      expect(convert_to_clocktime(4262)).to eq("01:11:02")
+    it ':convert_to_clocktime' do
+      expect(convert_to_clocktime(4262)).to eq('01:11:02')
     end
   end
 end
