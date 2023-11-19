3e707b54c33136f792560ca1ed3596e1182d1478
Joao Gilberto Saraiva <jgilbertons@gmail.com>
2023-11-19 17:06:40 -0300

    refactor: create run calculate and specs

---
+++ lib/run_calculate.rb
@@ -0,0 +1,14 @@
+# frozen_string_literal: true
+
+def calculate(calc_modal, time_or_pace, distance_or_pace)
+  case calc_modal
+  when 'p'
+    convert_to_clocktime((time_or_pace / distance_or_pace.to_i))
+  when 't'
+    convert_to_clocktime((time_or_pace * distance_or_pace.to_i))
+  when 'd'
+    (time_or_pace.to_f / convert_to_seconds(distance_or_pace)).round(2)
+  else
+    raise ArgumentError, 'You have to choose p (pace), t (time run) or d (distance).'
+  end
+end
