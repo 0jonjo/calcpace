f0a97688569b761606f564feff5d70439c4908e6
João Gilberto Saraiva <jgilbertons@gmail.com>
2023-11-30 21:14:35 -0300

    [15] minor refacts to methods be more readable (#16)
    * refactor: run files
    
    * feature: refact specs it texts

--- lib/run_calculate.rb
+++ lib/run_calculate.rb
@@ -3,9 +3,9 @@
 def calculate(calc_modal, time_or_pace, distance_or_pace)
   case calc_modal
   when 'p'
-    convert_to_clocktime((time_or_pace / distance_or_pace.to_f))
+    convert_to_clocktime(time_or_pace / distance_or_pace.to_f)
   when 't'
-    convert_to_clocktime((time_or_pace * distance_or_pace.to_f))
+    convert_to_clocktime(time_or_pace * distance_or_pace.to_f)
   when 'd'
     (time_or_pace.to_f / convert_to_seconds(distance_or_pace)).round(2)
   end
--- spec/run_calculate_spec.rb
+++ spec/run_calculate_spec.rb
@@ -6,15 +6,15 @@ require 'run_convert'
 
 describe 'run_calculate' do
   context 'result' do
-    it 'in correct pace' do
+    it 'calculates correct pace' do
       expect(calculate('p', 3600, 10)).to eq('00:06:00')
     end
 
-    it 'in correct time run ' do
+    it 'calculates correct time run' do
       expect(calculate('t', 300, 12)).to eq('01:00:00')
     end
 
-    it 'in correct distance' do
+    it 'calculates correct distance' do
       expect(calculate('d', 5400, '00:05:00')).to eq(18.0)
     end
   end
