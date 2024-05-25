fbd653a3feaaed3922472cbd24fcd251f2ff3341
Joao Gilberto Saraiva <jgilbertons@gmail.com>
2024-05-25 15:52:48 -0300

    remove manual tests

--- lib/calcpace.rb
+++ lib/calcpace.rb
@@ -11,14 +11,3 @@ class Calcpace
 
   def initialize; end
 end
-
-# Use module to convert a distance
-# puts Calcpace.new.convert_distance(10, 'km')
-
-# puts Calcpace.new.convert_distance(10, 'mi')
-
-# puts Calcpace.new.pace('01:00:00', 10)
-
-# puts Calcpace.new.total_time('00:05:00', 12)
-
-# puts Calcpace.new.distance('01:00:00', '00:05:00')
