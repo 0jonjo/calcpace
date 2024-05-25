ae1ef5f787e03312e2a92b41fadb0ecb8c800d08
Joao Gilberto Saraiva <jgilbertons@gmail.com>
2024-05-25 10:13:44 -0300

    refactor run convert to use as a module

--- lib/run_convert.rb
+++ lib/run_convert.rb
@@ -1,10 +1,21 @@
 # frozen_string_literal: true
 
-def convert_to_seconds(time)
-  hour, minute, seconds = time.split(':').map(&:to_i)
-  (hour * 3600) + (minute * 60) + seconds
-end
+module RunConvert
+  def convert_to_seconds(time)
+    hour, minute, seconds = time.split(':').map(&:to_i)
+    (hour * 3600) + (minute * 60) + seconds
+  end
 
-def convert_to_clocktime(seconds)
-  Time.at(seconds).utc.strftime('%H:%M:%S')
+  def convert_to_clocktime(seconds)
+    Time.at(seconds).utc.strftime('%H:%M:%S')
+  end
+
+  def convert_distance(unit, distance)
+    case unit
+    when 'km'
+      distance * 0.621371
+    when 'mi'
+      distance * 1.60934
+    end
+  end
 end
--- spec/run_convert_spec.rb
+++ spec/run_convert_spec.rb
@@ -2,14 +2,24 @@
 
 require 'run_convert'
 
-describe 'run_convert' do
-  context 'Convert tests' do
+RSpec.describe RunConvert do
+  include RunConvert
+
+  describe 'Convert tests' do
     it ':convert_to_seconds' do
       expect(convert_to_seconds('01:11:02')).to eq(4262)
     end
 
     it ':convert_to_clocktime' do
       expect(convert_to_clocktime(4262)).to eq('01:11:02')
+    end
+
+    it ':convert_distance km' do
+      expect(convert_distance('km', 10)).to eq(6.21371)
+    end
+
+    it ':convert_distance mi' do
+      expect(convert_distance('mi', 10)).to eq(16.0934)
     end
   end
 end
