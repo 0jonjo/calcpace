6c128b51c4947822cab821ea5320130c7c6c1b8b
Joao Gilberto Saraiva <jgilbertons@gmail.com>
2024-06-16 18:15:58 -0300

    add method to check seconds

--- lib/calcpace/checker.rb
+++ lib/calcpace/checker.rb
@@ -1,17 +1,16 @@
 # frozen_string_literal: true
 
 module Checker
-  def check_digits(time, distance)
-    check_digits_time(time)
-    check_digits_distance(distance)
-  end
-
-  def check_digits_distance(distance)
+  def check_distance(distance)
     raise 'It must be a X.X positive number' unless distance.positive?
   end
 
-  def check_digits_time(time_string)
+  def check_time(time_string)
     raise 'It must be a XX:XX:XX time' unless time_string =~ /\d{0,2}(:)*?\d{1,2}(:)\d{1,2}/
+  end
+
+  def check_second(second)
+    raise 'It must be a positive number' unless second.is_a?(Integer) && second.positive?
   end
 
   def check_unit(unit)
--- test/calcpace/test_checker.rb
+++ test/calcpace/test_checker.rb
@@ -8,25 +8,14 @@ class TestChecker < Minitest::Test
     @checker = Calcpace.new
   end
 
-  def test_check_digits_distance
-    assert_raises(RuntimeError) { @checker.check_digits_distance(-1) }
-    assert_raises(RuntimeError) { @checker.check_digits_distance(0) }
-    assert_nil @checker.check_digits_distance(1)
+  def test_check_distance
+    assert_raises(RuntimeError) { @checker.check_distance(-1) }
+    assert_raises(RuntimeError) { @checker.check_distance(0) }
+    assert_nil @checker.check_distance(1)
   end
 
-  def test_check_digits_time
-    assert_raises(RuntimeError) { @checker.check_digits_time('') }
-    assert_nil @checker.check_digits_time('00:00:00')
-  end
-
-  def test_check_digits
-    assert_raises(RuntimeError) { @checker.check_digits('00:00:00', 0) }
-    assert_raises(RuntimeError) { @checker.check_digits('', 1) }
-    assert_nil @checker.check_digits('00:00:00', 1)
-  end
-
-  def test_convert
-    assert_equal 6.21, @checker.convert(10, 'km')
-    assert_equal 16.09, @checker.convert(10, 'mi')
+  def test_check_time
+    assert_raises(RuntimeError) { @checker.check_time('') }
+    assert_nil @checker.check_time('00:00:00')
   end
 end
