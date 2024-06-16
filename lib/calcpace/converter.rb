2763ba2efaae34bb48c127cc2c30c56bc90a41e5
Joao Gilberto Saraiva <jgilbertons@gmail.com>
2024-06-16 18:19:05 -0300

    add name friendly methods to convert distance, clocktime and seconds

--- lib/calcpace/converter.rb
+++ lib/calcpace/converter.rb
@@ -1,6 +1,22 @@
 # frozen_string_literal: true
 
 module Converter
+  def to_seconds(time)
+    check_time(time)
+    convert_to_seconds(time)
+  end
+
+  def to_clocktime(seconds)
+    check_second(seconds)
+    convert_to_clocktime(seconds)
+  end
+
+  def convert(distance, unit)
+    check_distance(distance)
+    check_unit(unit)
+    convert_distance(distance, unit)
+  end
+
   def convert_to_seconds(time)
     hour, minute, seconds = time.split(':').map(&:to_i)
     (hour * 3600) + (minute * 60) + seconds
@@ -10,8 +26,7 @@ module Converter
     Time.at(seconds).utc.strftime('%H:%M:%S')
   end
 
-  # tem que chamar as checagens de digitos antes de chamar o convert_distance
-  def convert(distance, unit)
+  def convert_the_distance(distance, unit)
     case unit
     when 'km'
       (distance * 0.621371).round(2)
