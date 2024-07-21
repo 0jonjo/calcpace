3984854c98f374900c317874e2c4b2c8325739a8
Joao Gilberto Saraiva <jgilbertons@gmail.com>
2024-07-21 14:09:08 -0300

    Add bigdecimal option and methods to receive result in seconds or bigdecimal

--- lib/calcpace/calculator.rb
+++ lib/calcpace/calculator.rb
@@ -1,21 +1,40 @@
-# frozen_string_literal: true
+require 'bigdecimal'
 
 module Calculator
-  def pace(time, distance)
-    check_time(time)
-    check_distance(distance)
-    convert_to_clocktime(convert_to_seconds(time) / distance.to_f)
+  def pace(time, distance, bigdecimal = false)
+    pace_in_seconds = pace_seconds(time, distance, bigdecimal)
+    convert_to_clocktime(pace_in_seconds)
   end
 
-  def total_time(pace, distance)
-    check_time(pace)
+  def pace_seconds(time, distance, bigdecimal = false)
+    check_time(time)
     check_distance(distance)
-    convert_to_clocktime(convert_to_seconds(pace) * distance.to_f)
+    seconds = convert_to_seconds(time)
+    bigdecimal ? seconds / BigDecimal(distance.to_s) : seconds / distance
   end
 
-  def distance(time, pace)
+  def total_time(pace, distance, bigdecimal = false)
+    total_time_in_seconds = total_time_seconds(pace, distance, bigdecimal)
+    convert_to_clocktime(total_time_in_seconds)
+  end
+
+  def total_time_seconds(pace, distance, bigdecimal = false)
+    check_time(pace)
+    check_distance(distance)
+    pace_seconds = convert_to_seconds(pace)
+    bigdecimal ? pace_seconds * BigDecimal(distance.to_s) : pace_seconds * distance
+  end
+
+  def distance(time, pace, bigdecimal = false)
     check_time(time)
     check_time(pace)
-    convert_to_seconds(time).to_f / convert_to_seconds(pace).round(2)
+    if bigdecimal
+      time_seconds = BigDecimal(convert_to_seconds(time).to_s)
+      pace_seconds = BigDecimal(convert_to_seconds(pace).to_s)
+    else
+      time_seconds = convert_to_seconds(time)
+      pace_seconds = convert_to_seconds(pace)
+    end
+    time_seconds / pace_seconds
   end
 end
--- test/calcpace/test_calculator.rb
+++ test/calcpace/test_calculator.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 require 'minitest/autorun'
+require 'bigdecimal'
 require_relative '../../lib/calcpace'
 
 class TestCalculator < Minitest::Test
@@ -14,6 +15,23 @@ class TestCalculator < Minitest::Test
     assert_raises(RuntimeError) { @checker.pace('00:00:00', 0) }
     assert_raises(RuntimeError) { @checker.pace('00:00:00', -1) }
     assert_equal '00:06:00', @checker.pace('01:00:00', 10)
+    assert_equal '00:07:54', @checker.pace('01:37:21', 12.3)
+  end
+
+  def test_pace_without_bigdecimal_precision
+    assert_equal '00:07:54', @checker.pace('01:37:21', 12.3, false)
+  end
+
+  def test_pace_seconds
+    assert_raises(RuntimeError) { @checker.pace_seconds('', 10) }
+    assert_raises(RuntimeError) { @checker.pace_seconds('invalid', 10) }
+    assert_raises(RuntimeError) { @checker.pace_seconds('00:00:00', 0) }
+    assert_raises(RuntimeError) { @checker.pace_seconds('00:00:00', -1) }
+    assert_equal BigDecimal('474.8780487804878'), @checker.pace_seconds('01:37:21', 12.3)
+  end
+
+  def test_pace_seconds_with_bigdecimal_precision
+    assert_equal BigDecimal('0.474878048780487804878048780487804878049e3'), @checker.pace_seconds('01:37:21', 12.3, true)
   end
 
   def test_total_time
@@ -24,9 +42,27 @@ class TestCalculator < Minitest::Test
     assert_equal '01:00:00', @checker.total_time('00:05:00', 12)
   end
 
+  def test_total_time_seconds
+    assert_raises(RuntimeError) { @checker.total_time_seconds('', 10) }
+      assert_raises(RuntimeError) { @checker.total_time_seconds('invalid', 10) }
+    assert_raises(RuntimeError) { @checker.total_time_seconds('00:00:00', 0) }
+    assert_raises(RuntimeError) { @checker.total_time_seconds('00:00:00', -1) }
+    assert_equal 3600, @checker.total_time_seconds('00:05:00', 12)
+    assert_equal 71844.3, @checker.total_time_seconds('01:37:21', 12.3)
+  end
+
+  def test_total_time_seconds_with_bigdecimal_precision
+    assert_equal BigDecimal('0.718443e5'), @checker.total_time_seconds('01:37:21', 12.3, true)
+  end
+
   def test_distance
     assert_raises(RuntimeError) { @checker.distance('', '00:05:00') }
     assert_raises(RuntimeError) { @checker.distance('01:00:00', '') }
     assert_equal 18.0, @checker.distance('01:30:00', '00:05:00')
+    assert_equal 15.0, @checker.distance('01:37:21', '00:06:17')
+  end
+
+  def test_distance_with_bigdecimal_precision
+    assert_equal BigDecimal('0.15493368700265251989389920424403183024e2'), @checker.distance('01:37:21', '00:06:17', true)
   end
 end
