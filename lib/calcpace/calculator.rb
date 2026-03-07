22e9a0d2d6b0158ca5ce52aa4c95ebc2e3b7f920
Joao Gilberto Saraiva <jgilbertons@gmail.com>
2026-03-06 22:10:10 -0300

    chore: add methods for calculating pace and distance from time and velocity strings

--- lib/calcpace/calculator.rb
+++ lib/calcpace/calculator.rb
@@ -21,11 +21,31 @@ module Calculator
     distance.to_f / time
   end
 
+  # Calculates velocity from a time string
+  #
+  # @param time [String] time in HH:MM:SS or MM:SS format
+  # @param distance [Numeric] distance in any unit (e.g., meters, kilometers)
+  # @return [Float] velocity (distance/time)
+  # @raise [Calcpace::InvalidTimeFormatError] if time string is invalid
+  # @raise [Calcpace::NonPositiveInputError] if distance is not positive
+  #
+  # @example
+  #   checked_velocity('01:00:00', 10_000) #=> 2.778 (10000 meters / 3600 seconds)
   def checked_velocity(time, distance)
     seconds = convert_to_seconds(validate_time(time))
     velocity(seconds, distance)
   end
 
+  # Calculates velocity from a time string and returns result as a clock time string
+  #
+  # @param time [String] time in HH:MM:SS or MM:SS format
+  # @param distance [Numeric] distance in any unit
+  # @return [String] velocity in HH:MM:SS format
+  # @raise [Calcpace::InvalidTimeFormatError] if time string is invalid
+  # @raise [Calcpace::NonPositiveInputError] if distance is not positive
+  #
+  # @example
+  #   clock_velocity('01:00:00', 10_000) #=> '00:00:00'
   def clock_velocity(time, distance)
     convert_to_clocktime(checked_velocity(time, distance))
   end
@@ -44,34 +64,101 @@ module Calculator
     time.to_f / distance
   end
 
+  # Calculates pace from a time string
+  #
+  # @param time [String] time in HH:MM:SS or MM:SS format
+  # @param distance [Numeric] distance in any unit (e.g., kilometers, miles)
+  # @return [Float] pace (time/distance) in seconds
+  # @raise [Calcpace::InvalidTimeFormatError] if time string is invalid
+  # @raise [Calcpace::NonPositiveInputError] if distance is not positive
+  #
+  # @example
+  #   checked_pace('00:50:00', 10) #=> 300.0 (300 seconds/km = 5:00/km)
   def checked_pace(time, distance)
     seconds = convert_to_seconds(validate_time(time))
     pace(seconds, distance)
   end
 
+  # Calculates pace from a time string and returns result as a clock time string
+  #
+  # @param time [String] time in HH:MM:SS or MM:SS format
+  # @param distance [Numeric] distance in any unit
+  # @return [String] pace in HH:MM:SS format
+  # @raise [Calcpace::InvalidTimeFormatError] if time string is invalid
+  # @raise [Calcpace::NonPositiveInputError] if distance is not positive
+  #
+  # @example
+  #   clock_pace('00:50:00', 10) #=> '00:05:00'
   def clock_pace(time, distance)
     convert_to_clocktime(checked_pace(time, distance))
   end
 
+  # Calculates time given velocity and distance
+  #
+  # @param velocity [Numeric] velocity in any unit
+  # @param distance [Numeric] distance in any unit
+  # @return [Float] time (velocity * distance)
+  # @raise [Calcpace::NonPositiveInputError] if velocity or distance is not positive
+  #
+  # @example
+  #   time(300, 10) #=> 3000.0 (300 s/km * 10 km = 3000 seconds)
   def time(velocity, distance)
     validate_positive({ velocity: velocity, distance: distance })
     velocity * distance
   end
 
+  # Calculates time from a velocity string and distance
+  #
+  # @param velocity [String] velocity in HH:MM:SS or MM:SS format
+  # @param distance [Numeric] distance in any unit
+  # @return [Float] time in seconds
+  # @raise [Calcpace::InvalidTimeFormatError] if velocity string is invalid
+  # @raise [Calcpace::NonPositiveInputError] if distance is not positive
+  #
+  # @example
+  #   checked_time('05:00', 10) #=> 3000.0 (5:00/km * 10 km = 3000 seconds)
   def checked_time(velocity, distance)
     velocity_seconds = convert_to_seconds(validate_time(velocity))
     time(velocity_seconds, distance)
   end
 
+  # Calculates time from a velocity string and returns result as a clock time string
+  #
+  # @param velocity [String] velocity in HH:MM:SS or MM:SS format
+  # @param distance [Numeric] distance in any unit
+  # @return [String] time in HH:MM:SS format
+  # @raise [Calcpace::InvalidTimeFormatError] if velocity string is invalid
+  # @raise [Calcpace::NonPositiveInputError] if distance is not positive
+  #
+  # @example
+  #   clock_time('05:00', 10) #=> '00:50:00'
   def clock_time(velocity, distance)
     convert_to_clocktime(checked_time(velocity, distance))
   end
 
+  # Calculates distance given time and velocity
+  #
+  # @param time [Numeric] time in any unit
+  # @param velocity [Numeric] velocity in any unit
+  # @return [Float] distance (time/velocity)
+  # @raise [Calcpace::NonPositiveInputError] if time or velocity is not positive
+  #
+  # @example
+  #   distance(3000, 300) #=> 10.0 (3000 seconds / 300 s/km = 10 km)
   def distance(time, velocity)
     validate_positive({ time: time, velocity: velocity })
     time.to_f / velocity
   end
 
+  # Calculates distance from time and velocity strings
+  #
+  # @param time [String] time in HH:MM:SS or MM:SS format
+  # @param velocity [String] velocity in HH:MM:SS or MM:SS format
+  # @return [Float] distance
+  # @raise [Calcpace::InvalidTimeFormatError] if any string is invalid
+  #
+  # @example
+  #   checked_distance('00:50:00', '05:00') #=> 10.0 (50 min / 5:00/km = 10 km)
   def checked_distance(time, velocity)
     time_seconds = convert_to_seconds(validate_time(time))
     velocity_seconds = convert_to_seconds(validate_time(velocity))
