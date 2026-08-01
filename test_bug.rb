a337928fe49b6dac5adeea866cc4d03f03b3aaa7
Joao Gilberto Saraiva <jgilbertons@gmail.com>
2026-08-01 09:34:31 -0300

    chore: drop review debug scripts committed by mistake
    They were untracked scratch files from the review pass; git ls-files
    feeds the gemspec, so anything tracked at the root ships in the gem.

--- test_bug.rb
+++
@@ -1,4 +0,0 @@
-require './lib/calcpace'
-require './lib/calcpace/fitness_predictor'
-calc = Calcpace.new
-puts calc.predict_time_from_vo2max(50, 'marathon')
--- test_numeric_race.rb
+++
@@ -1,3 +0,0 @@
-require './lib/calcpace'
-calc = Calcpace.new
-puts calc.race_times_from_vo2max(50, races: [10.0, '5k']).inspect
